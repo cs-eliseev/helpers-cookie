b067cad6457d739ff9076235a210ea08b5de5689
cs-eliseev <a.k.eliseev@gmail.com>
2019-01-20 08:20:05 +0300

    add example set cookie

---
+++ examples/examples-cookie.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+use cse\helpers\Cookie;
+
+$key = 'cookie_example';
+
+// Example: set
+// 12345 => ['cookie_example' => 12345]
+Cookie::set($key, 12345);
