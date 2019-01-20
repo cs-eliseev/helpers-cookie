7027c7d927081da5d03cf60eb0f3d2b487c72f58
cs-eliseev <a.k.eliseev@gmail.com>
2019-01-20 08:19:41 +0300

    add method set cookie

---
+++ src/Cookie.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace cse\helpers;
+
+/**
+ * Class Cookie
+ *
+ * @package cse\helpers
+ */
+class Cookie
+{
+    const COOKIE_PATH    = '/';
+    const COOKIE_TIMEOUT = 432000;
+
+    /**
+     * Set cookie by name
+     *
+     * @param string $name
+     * @param $value
+     * @param string $path
+     * @param string $timeout
+     * @return bool
+     */
+    public static function set(string $name, $value, string $path = self::COOKIE_PATH, string $timeout = self::COOKIE_TIMEOUT): bool
+    {
+        $_COOKIE[$name] = $value;
+
+        return setcookie($name, $value, time() + $timeout, $path);
+    }
+}
